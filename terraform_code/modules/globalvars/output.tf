9f42a0c3ecc4b099774971146ad755cedbb07771
RH De Leon <147106158+rhrony1513@users.noreply.github.com>
2025-02-01 23:16:34 -0800

    Adding Terraform Code

---
+++ terraform_code/modules/globalvars/output.tf
@@ -0,0 +1,13 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "Rhony"
+    "App"   = "Web"
+    "Project" = "CLO835"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "clo835_assign1"
+}
